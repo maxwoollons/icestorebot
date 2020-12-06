e7de56b8e0a86f34231ef52346e797bd54334654
BuildTools <unconfigured@null.spigotmc.org>
2020-12-07 08:00:07 +1000

    Reco Fix

--- index.js
+++ index.js
@@ -203,26 +203,3 @@ function actions(){
 
 
 
-
-
-bot.on('error', function(err) {
-  console.log('Error attempting to reconnect: ' + err.errno + '.');
-  if (err.code == undefined) {
-      console.log('Invalid credentials OR bot needs to wait because it relogged too quickly.');
-      console.log('Will retry to connect in 30 seconds. ');
-      setTimeout(relog, 30000);
-  }
-});
-
-bot.on('end', function() {
-  console.log("Bot has ended");
-  // If set less than 30s you will get an invalid credentials error, which we handle above.
-  setTimeout(relog, 30000);  
-});
-
-
-function relog() {
-console.log("Attempting to reconnect...");
-StartUp()
-}
-
